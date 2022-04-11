a47e5a85ca2c128607d2dc334eb25dd561dee5bf
Marvin Rohde <marvin.rohde@leanix.net>
2022-04-11 10:49:25 +0200

    NMBS-1876 Add client generation workflow

---
+++ scripts/copy-generated-openapi-files.ts
@@ -0,0 +1,25 @@
+const fs = require('fs-extra');
+
+const pathMappings: Record<string, string> = {
+  integration: 'src/models'
+};
+
+const appName = process.argv[3];
+const filesSourceDir = process.argv[4];
+const appPath = pathMappings[appName];
+
+if (!fs.existsSync(appPath)) {
+  try {
+    fs.mkdirSync(appPath, { recursive: true });
+  } catch {
+    throw new Error(`Could not create directory ${appPath}`);
+  }
+}
+
+try {
+  fs.copySync(`${filesSourceDir}/models`, `${__dirname}/../${appPath}`);
+} catch (error) {
+  throw new Error(`Could not copy ${filesSourceDir} to ${appPath}`);
+}
+
+console.info(`copied ${filesSourceDir} to ${appPath}`);
