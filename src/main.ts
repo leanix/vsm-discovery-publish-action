4d67beba9628e454866867cb701fb25aa6041fde
Marvin Rohde <marvin.rohde@leanix.net>
2022-05-02 15:46:26 +0200

    NMBS-1877 Remove unused type

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,6 @@ import { KeyVaultManagementClient, Secret } from '@azure/arm-keyvault';
 import { ClientSecretCredential } from '@azure/identity';
 import validateIntegration from './integration.validator';
 
-type SecretStoreCredentials = {
-  ARM_CLIENT_ID: string;
-  ARM_CLIENT_SECRET: string;
-  ARM_SUBSCRIPTION_ID: string;
-  ARM_TENANT_ID: string;
-};
-
 const REGIONS: Readonly<Record<string, string>> = {
   westeurope: 'eu',
   eastus: 'us',
