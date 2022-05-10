fd8810caf3b47610f9566035a3cfb5ad0c0abb7f
Marvin Rohde <marvin.rohde@leanix.net>
2022-05-10 14:22:13 +0200

    NMBS-2025 Clear generated folder before copying

--- scripts/copy-generated-openapi-files.ts
+++ scripts/copy-generated-openapi-files.ts
@@ -8,12 +8,12 @@ const appName = process.argv[3];
 const filesSourceDir = process.argv[4];
 const appPath = pathMappings[appName];
 
-if (!fs.existsSync(appPath)) {
-  try {
-    fs.mkdirSync(appPath, { recursive: true });
-  } catch {
-    throw new Error(`Could not create directory ${appPath}`);
-  }
+fs.removeSync(appPath);
+
+try {
+  fs.mkdirSync(appPath, { recursive: true });
+} catch {
+  throw new Error(`Could not create directory ${appPath}`);
 }
 
 try {
