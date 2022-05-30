1ea08e3988de26564440b94a4d24a16fee7d5f0a
Marvin Rohde <marvin.rohde@leanix.net>
2022-05-30 12:41:54 +0200

    NMBS-2081 Add test for code snippet validation

--- test/unit/integration.validator.spec.ts
+++ test/unit/integration.validator.spec.ts
@@ -1,11 +1,27 @@
-import { expect, test } from '@jest/globals';
+import { expect } from '@jest/globals';
 import fs from 'fs-extra';
+import { cloneDeep } from 'lodash';
 import path from 'path';
+import { IntegrationRequestDto } from '../../src/models/integration-request-dto';
 import IntegrationValidator from '../../src/validators/integration.validator';
 
-test('validate integration', async () => {
-  const integrationJson = fs.readJsonSync(path.join(__dirname, '../testdata/integration-test.json'));
+describe('integration validation', () => {
+  const integrationJson: IntegrationRequestDto = fs.readJsonSync(path.join(__dirname, '../testdata/integration-test.json'));
   const integrationSchema = fs.readJsonSync(path.join(__dirname, '../testdata/integration.schema.json'));
   const integrationValidator = new IntegrationValidator();
-  await expect(integrationValidator.validate(integrationJson, integrationSchema)).resolves.toBeUndefined();
+
+  it('should accept a valid integration', async () => {
+    await expect(integrationValidator.validate(integrationJson, integrationSchema)).resolves.toEqual(true);
+  });
+
+  it('should show an error for invalid code snippets', async () => {
+    const invalidIntegration = cloneDeep(integrationJson);
+    invalidIntegration.pageSchemas[0].fields.push({
+      id: 'snippet',
+      type: 'CODE_SNIPPET',
+      label: 'snippet',
+      snippet: 'field3: $idonotexist, workspaceId: $workspaceId'
+    });
+    await expect(integrationValidator.validate(invalidIntegration, integrationSchema)).rejects.toBeDefined();
+  });
 });
