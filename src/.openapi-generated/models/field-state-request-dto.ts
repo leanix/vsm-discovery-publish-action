8fe2f07f64dab834e95c1b0693779ac06593cdb5
leanix-ci <leanix-ci@users.noreply.github.com>
2022-06-03 08:55:40 +0000

    Update OpenAPI client files for vsm-discovery

--- src/.openapi-generated/models/field-state-request-dto.ts
+++ src/.openapi-generated/models/field-state-request-dto.ts
@@ -6,6 +6,7 @@
  */
 export interface FieldStateRequestDto {
   id: string;
+  type: 'BOOLEAN' | 'CODE_SNIPPET' | 'EMAIL' | 'JSON' | 'NUMBER' | 'PASSWORD' | 'RADIO' | 'SCHEDULE' | 'STRING_ARRAY' | 'TEXT';
   value?: {
 };
 }
