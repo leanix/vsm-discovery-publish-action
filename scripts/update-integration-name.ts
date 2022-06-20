24c0d4179b5f0ef05830d6f9acd0bd29d697ee67
QuentinVaucherLeanIX <quentin.vaucher@leanix.net>
2022-06-20 15:19:23 +0200

    NMBS-2149: Add GH action to rename an integration name

---
+++ scripts/update-integration-name.ts
@@ -0,0 +1,34 @@
+import * as core from '@actions/core';
+import { throwErrorAndExit } from '../src/errors';
+import { IntegrationClient } from '../src/client/integration.client';
+import { LeanixRegionClient } from '../src/client/leanix-region.client';
+
+async function updateIntegrationName() {
+  const [oldName, newName] = getOldAndNewIntegrationNames();
+  const integrationClient = new IntegrationClient();
+  const lxRegionClient = new LeanixRegionClient();
+
+  for (const [region, regionId] of Object.entries(LeanixRegionClient.REGIONS)) {
+    const token = await lxRegionClient.getMtmTokenForRegion(region, regionId);
+
+    const integration = (await integrationClient.getIntegrations(regionId, token)).find((it) => it.name === oldName);
+    if (!integration) {
+      core.info(`Integration with name '${oldName}' does not exist in region '${region}' (update skipped)`);
+    } else {
+      integration.name = newName;
+      await integrationClient.upsertIntegration(regionId, integration, token);
+      core.info(`Integration name updated successfully in region ${region}`);
+    }
+  }
+}
+
+function getOldAndNewIntegrationNames(): [old: string, new: string] {
+  const oldIntegrationName = process.argv[2];
+  const newIntegrationName = process.argv[3];
+  if (!oldIntegrationName || !newIntegrationName) {
+    throwErrorAndExit(`Cannot get integration names from inputs`);
+  }
+  return [oldIntegrationName, newIntegrationName];
+}
+
+void updateIntegrationName();
