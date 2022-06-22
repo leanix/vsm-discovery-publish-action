4da8e96b75f1930934a6e0de65033781fae5a788
leanix-ci <leanix-ci@users.noreply.github.com>
2022-06-22 07:21:38 +0000

    Update OpenAPI client files for vsm-discovery

---
+++ src/.openapi-generated/models/run-configuration-response-dto.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+
+/**
+ * Model used to return the configuration of an integration.
+ */
+export interface RunConfigurationResponseDto {
+  runId: string;
+  status: string;
+}
